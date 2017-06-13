af2a54da5040008088a4e7cde26c17a38dfc28ea
Yu Watanabe <yunabe.public@gmail.com>
2017-06-13 23:22:51 +0900

    Add an example to write csv in Go.

--- tips/csv_test.go
+++ tips/csv_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"reflect"
 	"strconv"
@@ -48,5 +49,47 @@ func TestReadCSV(t *testing.T) {
 		{name: "Bob", age: 23},
 	}) {
 		t.Error("Unexpected result: ", persons)
+	}
+}
+
+func writeCSVToFile(path string, persons []person) (err error) {
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("Failed to create %s: %v", path, err)
+	}
+	defer func() {
+		if cerr := f.Close(); err == nil && cerr != nil {
+			err = cerr
+		}
+	}()
+	w := csv.NewWriter(f)
+	for _, p := range persons {
+		row := []string{p.name, strconv.Itoa(p.age)}
+		w.Write(row)
+	}
+	// Don't forget to call Flush!
+	w.Flush()
+	// Return w.Error(). Don't ignore an error occured in Flush().
+	return w.Error()
+}
+
+func TestWriteCSV(t *testing.T) {
+	defer func() {
+		if err := os.RemoveAll("testdata/tmp.csv"); err != nil {
+			t.Error(err)
+		}
+	}()
+	persons := []person{{name: "Taro", age: 8}, {name: "Jiro", age: 5}}
+	err := writeCSVToFile("testdata/tmp.csv", persons)
+	if err != nil {
+		t.Error(err)
+	}
+	b, err := ioutil.ReadFile("testdata/tmp.csv")
+	if err != nil {
+		t.Error(err)
+	}
+	content := string(b)
+	if content != "Taro,8\nJiro,5\n" {
+		t.Errorf("Unexpected content: %q", content)
 	}
 }
